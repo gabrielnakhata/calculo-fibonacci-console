342feafe59ac4eb9aac007add60d1f28ed16bc09
gabrielnakhata <gabrielnakata@gmail.com>
2021-07-03 20:50:58 -0300

    initial commit

---
+++ calculo-fibonacci-console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace calculo_fibonacci_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos imprimir a seguência de fibonacci, a seguência será número que você digitar...");
+            Console.ReadKey();
+            Console.WriteLine("Digite um número...");
+            int quantVezesSequenciaUsuario = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
